9165ed584071e3062d4dfa34d58198a53919eec3
Baumi <baumgart.patrick+git@gmail.com>
2019-04-30 20:40:55 +0200

    Django asks for a json now.
    using:
    http://localhost:8000/sensors/<ipadress>/ to ask for a response.
    
    At least works for my dummy-sensor runnig on localhost.

--- django/sensors/apps.py
+++ django/sensors/apps.py
@@ -1,11 +1,7 @@
 from django.apps import AppConfig
-import requests
 
 
 class SensorsConfig(AppConfig):
   name = 'sensors'
 
-  def getSensorData (self, IP):
-    response = requests.get('http://'+str(IP))
-    return response.status_code
 
--- django/sensors/views.py
+++ django/sensors/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+import requests
 
 def index(request):
     return HttpResponse("Hello, world. You're at the sensors index.")
+
+def sensor(request, sensorIP):
+    response = requests.get('http://'+str(sensorIP)+':2905')
+    return HttpResponse(response)
+
